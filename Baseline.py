8d368c45b93a40a81505394fa7b538f5e8cd8e7e
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2021-03-04 01:14:47 +0530

    adding newdata

--- Baseline.py
+++ Baseline.py
@@ -21,22 +21,20 @@ from sklearn.model_selection import train_test_split
 
 
 
-def baseline_linear( X, Y,num_epochs,batch_size, criterion ,optimizer,output ):
+def baseline_linear( X, Y,num_epochs,batch_size,learningRate, criterion ,output, test_size ):
     
-    trainbase, testbase = train_test_split(list(range(X.shape[0])), test_size=0.3)
-
+    trainbase, testbase = train_test_split(list(range(X.shape[0])), test_size=test_size)
     ds = PrepareData(X, y=Y, scale_X=True)
     input_size =len(X[1])
     train_set = DataLoader(ds, batch_size=batch_size,
                        sampler=SubsetRandomSampler(trainbase))
     
     test_set = DataLoader(ds, batch_size=batch_size,
-                      sampler=SubsetRandomSampler(testbase))
+    sampler=SubsetRandomSampler(testbase))
 
     model = linearRegression(input_size, output)
 
-    cost_func = criterion
-    optimizer = optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
     all_losses = []
     logging.info("Training of Baseline starting")    
@@ -52,21 +50,26 @@ def baseline_linear( X, Y,num_epochs,batch_size, criterion ,optimizer,output ):
         #==========Forward pass===============
 
             preds = model(_X)
-            lossbase = cost_func(preds, _y)
+            lossbas = criterion(preds, _y)
+            l2_reg = torch.tensor(0.)
+            for param in chain(model.parameters()):
+                l2_reg += torch.norm(param)
+            lossbase = lossbas+1 * l2_reg 
 
         #==========backward pass==============
 
             optimizer.zero_grad()
             lossbase.backward()
             optimizer.step()
+
         batch_losses.append(lossbase.item())
-        meanbatchloss = np.mean(np.sqrt(batch_losses)).round(3)
+        meanbatchloss = np.sqrt(np.mean(batch_losses)).round(3)
         
 
 
-        if e % 10 == 0:
-            print("Epoch [{}/{}], Batch loss: {}".format(e, num_epochs, meanbatchloss))
-    logging.info("Training of Baseline successful. To print losses uncomment the line 53 and 54")  
+#        if e % 10 == 0:
+#           print("Epoch [{}/{}], Batch loss: {}".format(e, num_epochs, meanbatchloss))
+    logging.info("Training of Baseline successful. To print losses uncomment the line 67 and 68")  
 
 # prepares model for inference when trained with a dropout layer
     logging.info("Baseline testing Starting")    
@@ -81,12 +84,11 @@ def baseline_linear( X, Y,num_epochs,batch_size, criterion ,optimizer,output ):
 
     #apply modeltest_loss
         test_preds = model(_X)
-        test_loss = cost_func(test_preds, _y)
+        test_loss = criterion(test_preds, _y)
         
         test_batch_loss.append(test_loss.item())
         #print("Batch loss on test: {}".format(test_loss.item()))
         #loss_avg.update(test_loss.item())
-    meanbatchloss_test = np.mean(np.sqrt(batch_losses)).round(3)
+    meanbatchloss_test = np.sqrt(np.mean(batch_losses)).round(3)
     logging.info("baseline loss on test: {}".format(meanbatchloss_test)) 
-    for param in model.parameters():
-        print(param) 
+
--- train.py
+++ train.py
@@ -10,10 +10,12 @@ from tqdm import trange
 from torch.autograd import Variable
 import numpy as np
 import sklearn.preprocessing
+from itertools import chain
+import random
 
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
-
+from Baseline import baseline_linear
 from dataloader import PrepareData
 from model import linearRegression
 from sklearn.preprocessing import StandardScaler
@@ -26,21 +28,20 @@ parser.add_argument('--restore_dir', default=None,
                     help="Optional, name of the directory containing weights to reload before training, e.g., 'experiments/")
 parser.add_argument('--outputDim', type=int, default=1, help="provides outputdimension")
 
-parser.add_argument('--learningRate',type=int,default=0.001, help="define the learningrate")
+parser.add_argument('--learningRate',type=int,default=0.0001, help="define the learningrate")
 parser.add_argument("--epochs", type=int,default=100, help="Number of Epoches")
 parser.add_argument("--test_trainsplit", default=0.3)
 parser.add_argument("--batch_size",type=int, default=32)
 
 # for now we are not storing pretrained weigths
-
 def train_epoches(model,model2, train_set1,train_set2,unknow_point, epochs, criterion ,optimizer, restore_dir=None):
     all_losses = []
     for epoch in range(epochs):
 
         optimizer.zero_grad()
-## ToDO 
-# make below part parametric
-    # get output from the model, given the inputs
+        ## ToDO 
+        # make below part parametric
+        # get output from the model, given the inputs
         batch_losses = []
         for ix, ((Xb1, yb1), (Xb2, yb2)) in enumerate(zip(train_set1,train_set2)):
 
@@ -51,28 +52,30 @@ def train_epoches(model,model2, train_set1,train_set2,unknow_point, epochs, crit
             
             outputs1 = model(_X1)
             outputs2 = model2(_X2)
-    # get loss for the predicted output
+            # get loss for the predicted output
 
             loss1 = criterion(outputs1, _y1)+criterion(outputs2, _y2)
             
             l2_reg = torch.tensor(0.)
             for param in chain(model.parameters(), model2.parameters()):
                 l2_reg += torch.norm(param)
-            loss = loss1+ 2 * l2_reg + 0.0005*(criterion(model(unknow_point[:400,:len(_X1[1])]), model2(unknow_point[:400,-(len(_X2[1])):]))) #co-regularsation term
+            loss = loss1+ 1 * l2_reg + 0.0001*(criterion(model(unknow_point[:100,:len(_X1[1])]), model2(unknow_point[:100,-(len(_X2[1])):]))) #co-regularsation term
+        
         #nn.utils.clip_grad_norm_(parameters=chain(model.parameters(), model2.parameters()), max_norm=params.clip_grad) 
-    #adding loss for each functions 
-    # get gradients w.r.t to parameters
+        #adding loss for each functions 
+        # get gradients w.r.t to parameters
             loss.backward()
-    # update parameters
+        
+        # update parameters
             optimizer.step()
             batch_losses.append(loss.item())
             all_losses.append(loss.item())
-            #print(batch_losses)
-        meanbatchloss = np.mean(np.sqrt(batch_losses)).round(3)
-        logging.info("co-RMS-loss: {:05.2f}".format(meanbatchloss))
-       # print('epoch {}, loss {}'.format(epoch, meanbatchloss/2))
 
+        #print(batch_losses)
+        meanbatchloss = np.sqrt(np.mean(batch_losses)).round(3)
+        logging.info("co-RMS-loss: {:05.2f}".format(meanbatchloss))
         
+        #print('epoch {}, loss {}'.format(epoch, meanbatchloss/2))
         # to keep the loss fair divide by m
         ## avoiding m factor in batch loss
 
@@ -89,7 +92,10 @@ def load_datafile( dataset_path, multiReg = 1):
 
 def test_epochs(model,model2, test_set1,test_set2, criterion ,optimizer, restore_dir=None):
     
-    test_batch_losses = []
+    model.eval()
+    model2.eval()
+    all_losses = []
+
     for (Xb1, yb1),(Xb2 , yb2) in zip(test_set1,test_set2):
 
         _X1 = Variable(Xb1).float()
@@ -106,11 +112,14 @@ def test_epochs(model,model2, test_set1,test_set2, criterion ,optimizer, restore
         L1 = criterion(predicted,_y1)
         L2 =criterion(predicted2,_y2)
         total_error = (L1 + L2)/2
-        test_batch_losses.append(total_error.item())
-
-
+        #loss_avg.update(total_error.item())
+        all_losses.append(total_error.item())
+    
+    meanbatchloss = np.sqrt(np.mean(all_losses)).round(3)
+    
     #print(total_error)
-    logging.info("combine-rms on test set: {:05.2f}".format(total_error))
+    logging.info("combine-rms on test set: {:05.2f}".format(meanbatchloss))
+    
     print("done")
 
 if (__name__ == "__main__"):
@@ -133,30 +142,29 @@ if (__name__ == "__main__"):
     data_dir = args.dataset
 
     X, Y = load_datafile(args.dataset, args.outputDim)
-
     # collecting indices for test and train sets
-    train, test = train_test_split(list(range(X.shape[0])), test_size=args.test_trainsplit)
+    train_idx, test_idx = train_test_split(list(range(X.shape[0])), test_size=args.test_trainsplit)
     
     # need to automate for random points
-    
-    X1 = X[:,:6]
-    #Y = Y[:]
-    X2 = X[:,7:]
+    n = random.randint(1,len(X[0]))
+    #print(n)
+    X1 = X[:,:n]
+    X2 = X[:,n:]
     unseen_point = X[:,:]
-    #print(Y.shape)
 
     ds1 = PrepareData(X1, y=Y, scale_X=True)
     ds2 = PrepareData(X2, y=Y, scale_X=True)
+    
     # Load training data and test data
     # TODO find better way of doing 
     train_set1 = DataLoader(ds1, batch_size=batch_size,
-                       sampler=SubsetRandomSampler(train))
+                       sampler=SubsetRandomSampler(train_idx))
     train_set2 = DataLoader(ds2, batch_size=batch_size,
-                      sampler=SubsetRandomSampler(train))
+                      sampler=SubsetRandomSampler(train_idx))
     test_set1 = DataLoader(ds1, batch_size=batch_size,
-                      sampler=SubsetRandomSampler(test))
+                      sampler=SubsetRandomSampler(test_idx))
     test_set2 = DataLoader(ds2, batch_size=batch_size,
-                      sampler=SubsetRandomSampler(test))
+                      sampler=SubsetRandomSampler(test_idx))
     unseen_point = torch.from_numpy(unseen_point.astype(np.float32))
 
     #X1_train,X2_train, Y_train, X1_test,X2_test, Y_test, unseen_point = load_housing(data_dir,False)     
@@ -171,6 +179,8 @@ if (__name__ == "__main__"):
     outputDim = args.outputDim      # takes variable 'y'
     learningRate = args.learningRate 
     epochs = args.epochs
+    
+    
     ## TODO
     # need to make parametric to input other kernals
     model = linearRegression(inputDim1, outputDim)
@@ -178,7 +188,6 @@ if (__name__ == "__main__"):
 
     if torch.cuda.is_available():
         model.cuda()
-    from itertools import chain
 
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(chain(model.parameters(), model2.parameters()), lr=learningRate)
@@ -188,3 +197,5 @@ if (__name__ == "__main__"):
     train_epoches(model,model2, train_set1,train_set2, unseen_point, epochs, criterion ,optimizer, restore_dir=None)
     logging.info("Starting testing for epoch(s)")    
     test_epochs(model,model2, test_set1,test_set2, criterion ,optimizer, restore_dir=None)
+
+    baseline_linear(X, Y,epochs,batch_size,learningRate, criterion ,outputDim, args.test_trainsplit)
